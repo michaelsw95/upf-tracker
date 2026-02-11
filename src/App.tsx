eb609fe25718bde8a0266e9a7cd48a1be4fdfc3d
Michael Hancock <michael.hancock@protonmail.com>
2026-02-11 20:22:42 +0000

    Add initial react/vite project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-900 text-white p-6">
+      <h1 className="text-3xl font-bold">UPF Tracker</h1>
+        <p className="mt-4 text-slate-300">
+          Test
+        </p>
+    </div>
+  )
+}
+
+export default App
